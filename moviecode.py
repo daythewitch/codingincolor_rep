cad9272067c16466ab346a0ce5aed0b806d1ddac
Day <113803703+daythewitch@users.noreply.github.com>
2023-07-13 16:47:44 -0700

    Add files via upload
    Data Science Lesson

---
+++ moviecode.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv('Netflix_Revenue.csv')
+
+dates = pd.to_datetime(df['Date'], dayfirst=True)
+uacn_revenue = df['UACN Revenue']
+
+model = LinearRegression()
+
+X = dates.astype('int64').values.reshape(-1, 1)
+model.fit(X, uacn_revenue)
+
+predicted_revenue = model.predict(X)
+
+mse = mean_squared_error(uacn_revenue, predicted_revenue)
+rmse = mse ** 0.5
+
+print("Root Mean Squared Eroor:", rmse)
+
+plt.plot(dates, uacn_revenue, label='ActualRevenue')
+plt.plot(dates, predicted_revenue, label='Predicted Revenue')
+plt.legend()
+
+plt.xticks(rotation=45)
+
+plt.show()
